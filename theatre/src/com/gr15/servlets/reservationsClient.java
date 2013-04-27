a58aafc128a1a12803b3aa560328db23e5a88bac
pailloue <emilie.paillous@ensimag.fr>
2013-04-27 18:38:34 +0200

    transformation mesReservations en reservationsClient

--- theatre/src/com/gr15/servlets/reservationsClient.java
+++ theatre/src/com/gr15/servlets/reservationsClient.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class mesReservations
  */
 @WebServlet("/mesReservations")
-public class mesReservations extends HttpServlet {
+public class reservationsClient extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public mesReservations() {
+    public reservationsClient() {
         super();
         // TODO Auto-generated constructor stub
     }
