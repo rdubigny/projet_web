1648f51230232863ad8cc58c6027d5494f23f218
milloura <alice.millour@ensimag.fr>
2013-04-28 16:32:25 +0200

    tables version

--- theatre/WebContent/creationTables.sql
+++ theatre/WebContent/creationTables.sql
@@ -65,8 +65,10 @@ FOREIGN KEY (id_zone) REFERENCES zone(id_zone) ON DELETE CASCADE
 CREATE TABLE projweb_db.ticket(
 id_place INTEGER AUTO_INCREMENT,
 moment_vente DATETIME,
-PRIMARY KEY ( id_place),
-FOREIGN KEY (id_place) REFERENCES place(id_place) ON DELETE CASCADE
+id_representation INTEGER,
+PRIMARY KEY (id_place),
+FOREIGN KEY (id_place) REFERENCES place(id_place) ON DELETE CASCADE,
+FOREIGN KEY (id_representation) REFERENCES representation(id_representation) ON DELETE CASCADE
 ) ENGINE = INNODB;
 
 -- Associations --------------
@@ -80,5 +82,37 @@ id_dossier INTEGER,
 PRIMARY KEY ( id_reservation ),
 FOREIGN KEY (id_place) REFERENCES place(id_place) ON DELETE CASCADE,
 FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE,
+FOREIGN KEY (id_dossier) REFERENCES dossier(id_dossier) ON DELETE CASCADE
+) ENGINE = INNODB;
+
+
+
+• reservation(id_reservation, id_place, id_utilisateur)
+-  id_reservation > 0
+- id_place clef étrangère référence à place(id_place).
+- id_utilisateur clef étrangère référence à utilisateur(id_utilisateur).
+
+CREATE TABLE projweb_db.reservation(
+id_reservation INTEGER AUTO_INCREMENT,
+id_representation INTEGER,
+id_place INTEGER,
+id_utilisateur INTEGER,
+PRIMARY KEY ( id_reservation ),
+FOREIGN KEY (id_place) REFERENCES place(id_place) ON DELETE CASCADE,
+FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE,
+FOREIGN KEY (id_representation) REFERENCES representation(id_representation) ON DELETE CASCADE
+) ENGINE = INNODB;
+
+
+CREATE TABLE projweb_db.reservation(
+id_achat INTEGER AUTO_INCREMENT,
+id_representation INTEGER,
+id_place INTEGER,
+id_dossier INTEGER,
+id_utilisateur INTEGER,
+PRIMARY KEY ( id_reservation ),
+FOREIGN KEY (id_place) REFERENCES place(id_place) ON DELETE CASCADE,
+FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur) ON DELETE CASCADE,
+FOREIGN KEY (id_representation) REFERENCES representation(id_representation) ON DELETE CASCADE,
 FOREIGN KEY (id_dossier) REFERENCES dossier(id_dossier) ON DELETE CASCADE
 ) ENGINE = INNODB;
