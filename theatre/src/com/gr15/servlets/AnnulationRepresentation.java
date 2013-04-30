3ebf60659514d68ed8964aa99b18169ebca90129
pailloue <emilie.paillous@ensimag.fr>
2013-04-30 16:09:52 +0200

    bonjour les amis, je montre à léa comment fonctionne le git, alors je vous laisse un message, pour vous dire que je vous aime, et que j'ai modifié la page d'annulation d'une représentation :) voilà et là je vais pusher, haha

---
+++ theatre/src/com/gr15/servlets/AnnulationRepresentation.java
@@ -0,0 +1,39 @@
+package com.gr15.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AnnulationRepresentation
+ */
+@WebServlet( "/annulationRepresentation" )
+public class AnnulationRepresentation extends HttpServlet {
+    private static final long  serialVersionUID = 1L;
+    public static final String VUE              = "/WEB-INF/annulationRepresentation.jsp";
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException,
+            IOException {
+        // TODO Auto-generated method stub
+        this.getServletContext().getRequestDispatcher( VUE )
+                .forward( request, response );
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException,
+            IOException {
+        // TODO Auto-generated method stub
+    }
+
+}
